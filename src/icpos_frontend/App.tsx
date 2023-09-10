452816e10eba0892c7e88c12e8c6bb46a415951f
Kristofer <kristofer@fmckl.se>
2023-09-10 22:46:45 +0200

    Print styles

--- src/icpos_frontend/App.tsx
+++ src/icpos_frontend/App.tsx
@@ -10,14 +10,14 @@ import { router } from "./router";
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-gradient-to-br from-gray-200 to-gray-500 md:flex md:flex-col md:items-center md:justify-center">
-        <div className="container flex flex-col max-w-lg print:w-full print:max-w-none min-h-screen p-0 m-0 mx-auto prose prose-xl text-black md:rounded-lg bg-slate-50 md:shadow-xl md:h-[750px] md:min-h-0">
+      <div className="min-h-screen bg-gradient-to-br from-gray-200 to-gray-500 md:flex md:flex-col md:items-center md:justify-center print-bg">
+        <div className="container flex flex-col max-w-lg print:w-full print:max-w-none min-h-screen p-0 m-0 mx-auto prose prose-xl text-black md:rounded-lg bg-slate-50 md:shadow-xl md:h-[750px] md:min-h-0 print-maincontainer">
           <React.Suspense fallback={<FullpageLoading />}>
             <RouterProvider router={router} />
             <Toaster />
           </React.Suspense>
         </div>
-        <div className="invisible w-full mt-5 text-center text-gray-600 md:visible">
+        <div className="invisible w-full mt-5 text-center text-gray-600 md:visible print:hidden">
           Developed by:{" "}
           <a
             href="https://github.com/kristoferlund"
@@ -27,7 +27,7 @@ function App() {
             Kristofer Lund
           </a>
         </div>
-        <div className="invisible w-full mt-3 text-center text-gray-600 md:visible">
+        <div className="invisible w-full mt-3 text-center text-gray-600 md:visible print:hidden">
           <a
             href="https://github.com/kristoferlund/ic-pos"
             target="_blank"
